2aecfdbc6b53b9c2e306177550dc7791a7ab4bd5
Fabrice Semti <fsemti@sportiinggroup.co.uk>
2024-05-09 06:24:22 +0000

    remove languages WE do not use (and likely won't in the future

--- flake.nix
+++ flake.nix
@@ -96,91 +96,12 @@
 
     {
       templates = rec {
-        c-cpp = {
-          path = ./c-cpp;
-          description = "C/C++ development environment";
-        };
-
-        clojure = {
-          path = ./clojure;
-          description = "Clojure development environment";
-        };
-
-        csharp = {
-          path = ./csharp;
-          description = "C# development environment";
-        };
-
-        cue = {
-          path = ./cue;
-          description = "Cue development environment";
-        };
-
-        dhall = {
-          path = ./dhall;
-          description = "Dhall development environment";
-        };
-
-        elixir = {
-          path = ./elixir;
-          description = "Elixir development environment";
-        };
-
-        elm = {
-          path = ./elm;
-          description = "Elm development environment";
-        };
-
+       
         empty = {
           path = ./empty;
           description = "Empty dev template that you can customize at will";
         };
-
-        gleam = {
-          path = ./gleam;
-          description = "Gleam development environment";
-        };
-
-        go = {
-          path = ./go;
-          description = "Go (Golang) development environment";
-        };
-
-        hashi = {
-          path = ./hashi;
-          description = "HashiCorp DevOps tools development environment";
-        };
-
-        haskell = {
-          path = ./haskell;
-          description = "Haskell development environment";
-        };
-
-        java = {
-          path = ./java;
-          description = "Java development environment";
-        };
-
-        kotlin = {
-          path = ./kotlin;
-          description = "Kotlin development environment";
-        };
-
-        latex = {
-          path = ./latex;
-          description = "LaTeX development environment";
-        };
-
-        nickel = {
-          path = ./nickel;
-          description = "Nickel development environment";
-        };
-
-        nim = {
-          path = ./nim;
-          description = "Nim development environment";
-        };
-
+        
         nix = {
           path = ./nix;
           description = "Nix development environment";
@@ -189,32 +110,12 @@
         node = {
           path = ./node;
           description = "Node.js development environment";
-        };
-
-        opa = {
-          path = ./opa;
-          description = "Open Policy Agent development environment";
-        };
-
-        php = {
-          path = ./php;
-          description = "PHP development environment";
-        };
-
-        protobuf = {
-          path = ./protobuf;
-          description = "Protobuf development environment";
-        };
+        };        
 
         pulumi = {
           path = ./pulumi;
           description = "Pulumi development environment";
-        };
-
-        purescript = {
-          path = ./purescript;
-          description = "Purescript development environment";
-        };
+        };      
 
         python = {
           path = ./python;
@@ -224,22 +125,7 @@
         ruby = {
           path = ./ruby;
           description = "Ruby development environment";
-        };
-
-        rust = {
-          path = ./rust;
-          description = "Rust development environment";
-        };
-
-        rust-toolchain = {
-          path = ./rust-toolchain;
-          description = "Rust development environment with Rust version defined by a rust-toolchain.toml file";
-        };
-
-        scala = {
-          path = ./scala;
-          description = "Scala development environment";
-        };
+        };        
 
         shell = {
           path = ./shell;
@@ -251,20 +137,10 @@
           description = "Terraform development environment";
         };        
 
-        vlang = {
-          path = ./vlang;
-          description = "Vlang developent environment";
-        };
-
-        zig = {
-          path = ./zig;
-          description = "Zig development environment";
-        };
-
-        # Aliases
-        rt = rust-toolchain;
-        c = c-cpp;
-        cpp = c-cpp;
+        # # Aliases
+        # rt = rust-toolchain;
+        # c = c-cpp;
+        # cpp = c-cpp;
       };
     };
 }
