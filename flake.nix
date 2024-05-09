ac975616af39dc1598d126a2af9a42f0a874f5b0
Fabrice Semti <fsemti@sportiinggroup.co.uk>
2024-05-09 08:01:10 +0000

    upgrade python

--- flake.nix
+++ flake.nix
@@ -76,7 +76,7 @@
       ];
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit overlays system; };
+        pkgs = import nixpkgs { inherit overlays system; config.allowUnsupportedSystem = true; };
       });
     in
     {
